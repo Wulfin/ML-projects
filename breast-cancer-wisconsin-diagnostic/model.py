d6e1f6e6eafe03cbc7d02c825d5b20c0d07872a2
Wulfin <saifelislambettaoui1955@gmail.com>
2023-12-09 18:48:04 +0100

    Model training completed and starting improving the model

--- breast-cancer-wisconsin-diagnostic/model.py
+++ breast-cancer-wisconsin-diagnostic/model.py
@@ -33,7 +33,7 @@ df = pd.DataFrame(np.c_[cancer['data'], cancer['target']], columns=np.append(can
 # print(df['target'].value_counts())
 # print(357 / len(df), 211/ len(df))
 
-sns.countplot(data=df, x='target')
+#sns.countplot(data=df, x='target')
 plt.savefig('countplot.png')  # Save as PNG image
 # plt.show()
 
@@ -41,14 +41,13 @@ print(df.columns)
 # sns.pairplot(data=df, vars=['mean radius', 'mean texture', 'mean perimeter', 'mean area','mean smoothness'], hue='target')
 # plt.show()
 
-plt.figure(figsize=(20,15))
+#plt.figure(figsize=(20,15))
 # sns.heatmap(df.corr(), annot=True, cmap='viridis_r')
 # plt.show()
 
 print(df.corr()['target'].sort_values(ascending=False))
 df.corr()['target'][:-1].sort_values().plot(kind='bar')
 # plt.show()
-
 
 
 
@@ -61,18 +60,29 @@ X = df.drop('target', axis=1)
 y = df['target']
 
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-
 # Model Training
 print("\n\nModel Training")
-from sklearn.svm import SVC
 svc_model = SVC()
 svc_model.fit(X_train, y_train)
 
-
 # Model Evaluation
 y_preds = svc_model.predict(X_test)
-from sklearn.metrics import classification_report, confusion_matrix, pair_confusion_matrix
 print(classification_report(y_test, y_preds))
+
+cm = confusion_matrix(y_test, y_preds)
+print(cm)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.show()
+
+
+
+
+# IMPORTANT: Improving the model
